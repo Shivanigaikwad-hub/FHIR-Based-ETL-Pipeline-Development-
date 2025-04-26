6d2c6f2f8cded11120068b6bd20d18f2a9fbaae6
Gaikwad, Shivani <shigaik@iu.edu>
2025-04-25 22:22:19 -0400

    Removed all unnecessary files

--- README.md
+++
@@ -1,5 +0,0 @@
-# Group_Project
-
-The website can be accessed through this link:
-
-[Group Project Website](https://pages.github.iu.edu/shigaik/Group_Project/)
--- credentials.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from dotenv import load_dotenv, find_dotenv
-
-# Load environment variables from a .env file if it exists
-load_dotenv(find_dotenv())
-
-BASE_SERVER_URL = os.getenv("BASE_SERVER_URL")
-OPENEMR_API_URL = os.getenv("https://in-info-web20.luddy.indianapolis.iu.edu/interface/main/tabs/main.php?token_main=qzu0NCc8VlRrjDcqfBHaF8z4Jyd1Ny8VOOBOKaJn")
